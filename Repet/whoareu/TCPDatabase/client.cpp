bf48f2e5c7bc4659f1c8eb162e447ae4de3d4003
Morgan-iv <ivushkinalex@gmail.com>
2018-05-25 10:50:43 +0300

    update TCPDB

--- Repet/whoareu/TCPDatabase/client.cpp
+++ Repet/whoareu/TCPDatabase/client.cpp
@@ -15,12 +15,23 @@ int tcp_port = 55555;
 
 using response_t = std::vector<record_t>;
 
-using namespace std;
+//using namespace std;
 
-response_t DBQuery(std::string querystr)
+void print(const record_t & rec)
 {
-	struct sockaddr_in addr;
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
+    std::cout << rec.ID << std::endl
+              << rec.journal << std::endl
+              << rec.year << std::endl
+              << rec.number << std::endl
+              << rec.author << std::endl
+              << rec.article << std::endl
+              << std::endl;
+}
+
+int main()
+{
+    struct sockaddr_in addr;
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if(sock < 0)
     {
         perror("socket");
@@ -39,63 +50,67 @@ response_t DBQuery(std::string querystr)
     int chk;
     recvall(sock, (char *)&chk, sizeof(int), MSG_NOSIGNAL);
     if (chk == 1)
-    	chk = 0;
-	sendall(sock, (char *)&chk, sizeof(int), MSG_NOSIGNAL);
-    
-    int len = querystr.length();
-    sendall(sock, (char *)&len, sizeof(int), MSG_NOSIGNAL);
-    sendall(sock, querystr.c_str(), len, MSG_NOSIGNAL);
+        chk = 0;
+    sendall(sock, (char *)&chk, sizeof(int), MSG_NOSIGNAL);
 
-    recvall(sock, (char *)&len, sizeof(int), MSG_NOSIGNAL);
-    if (chk)
-    	len = swapbytes(len);
-    if (len < 0)
-    {
-        cout << "exception" << len << endl << endl;
-        shutdown(sock, 2);
-        close(sock);
-        return response_t(0);
-    }
-    response_t result(len);
-    if (len)
-        recvall(sock, (char *)result.data(), len * sizeof(record_t), MSG_NOSIGNAL);
-    if (chk)
-    	for (int i = 0; i < len; ++i)
-    	{
-    		result[i].ID = swapbytes(result[i].ID);
-    		result[i].year = swapbytes(result[i].year);
-    		result[i].number = swapbytes(result[i].number);
-    	}
-    shutdown(sock, 2);
-    close(sock);
-    return result;
-}
+    int comchar = 1;
 
-void print(const record_t & rec)
-{
-    cout << rec.ID << endl
-         << rec.journal << endl
-         << rec.year << endl
-         << rec.number << endl
-         << rec.author << endl
-         << rec.article << endl
-         << endl;
-}
-
-int main()
-{
-	//response_t res = DBQuery(std::string("select author=[Au1] number =[42,42]"));
 	while (true)
     {
-        string q;
-        cout << '>';
-        std::getline(cin, q);
+        if (!std::cin.good())
+            break;
+        
+        std::string q;
+        if (comchar)
+            std::cout << '>';
+        std::getline(std::cin, q);
+
         if (!q.compare("exit"))
             break;
-        response_t res = DBQuery(q);
-        for (int i = 0; i < res.size(); ++i)
+
+        if (!q.compare("nochar"))
         {
-            print(res[i]);
+            comchar = 0;
+            continue;
+        }
+
+        if (!q.compare("onchar"))
+        {
+            comchar = 1;
+            continue;
+        }
+
+        int len = q.length();
+        sendall(sock, (char *)&len, sizeof(int), MSG_NOSIGNAL);
+        sendall(sock, q.c_str(), len, MSG_NOSIGNAL);
+    
+        recvall(sock, (char *)&len, sizeof(int), MSG_NOSIGNAL);
+        if (chk)
+            len = swapbytes(len);
+        if (len < 0)
+        {
+            std::cout << "exception" << len << std::endl << std::endl;
+            continue;
+        }
+
+        response_t result(len);
+        if (len)
+            recvall(sock, (char *)result.data(), len * sizeof(record_t), MSG_NOSIGNAL);
+        if (chk)
+            for (int i = 0; i < len; ++i)
+            {
+                result[i].ID = swapbytes(result[i].ID);
+                result[i].year = swapbytes(result[i].year);
+                result[i].number = swapbytes(result[i].number);
+            }
+        for (size_t i = 0; i < result.size(); ++i)
+        {
+            print(result[i]);
         }
     }
+    chk = 0;
+    sendall(sock, (char *)&chk, sizeof(int), MSG_NOSIGNAL);
+    shutdown(sock, 2);
+    close(sock);
+    return 0;
 }
--- Repet/whoareu/TCPDatabase/server.cpp
+++ Repet/whoareu/TCPDatabase/server.cpp
@@ -85,12 +85,26 @@ const size_t block_size = 1 * read_size;
 const int32_t max_com_len = 4096;
 uint max_thread;
 
-const int query_pos_type = 1;
-const int query_pos_author = 1;
-const int query_pos_year1 = 2;
-const int query_pos_year2 = 3;
-const int query_pos_number1 = 4;
-const int query_pos_number2 = 5;
+const int select_pos_author = 1;
+const int select_pos_year1 = 2;
+const int select_pos_year2 = 3;
+const int select_pos_number1 = 4;
+const int select_pos_number2 = 5;
+
+const int remove_pos_ID = 1;
+
+const int update_pos_ID = 1;
+const int update_pos_author = 2;
+const int update_pos_year = 3;
+const int update_pos_number = 4;
+const int update_pos_journal = 5;
+const int update_pos_article = 6;
+
+const int insert_pos_author = 1;
+const int insert_pos_year = 2;
+const int insert_pos_number = 3;
+const int insert_pos_journal = 4;
+const int insert_pos_article = 5;
 
 const char fnamemeta[] = "filemeta";
 const char fnamedata[] = "filedata";
@@ -99,7 +113,10 @@ const char fnamekeysmap[] = "filekeysmap";
 const char fnameyearmap[] = "fileyearmap";
 const char fnamenumbmap[] = "filenumbmap";
 
-const char select_regex[] = " *select *(?>author *= *\\[(.+?)\\] *)?(?:year *= *\\[ *(\\d+) *, *(\\d+) *\\] *)?(?:number *= *\\[ *(\\d+) *, *(\\d+) *\\] *)?";
+const boost::regex re_select(" *select *(?>author *= *\\[(.{1,31}?)\\] *)?(?:year *= *\\[ *(\\d+) *, *(\\d+) *\\] *)?(?:number *= *\\[ *(\\d+) *, *(\\d+) *\\] *)?");
+const boost::regex re_update(" *update *ID *= *\\[ *(\\d+) *\\] *(?>author *= *\\[(.{1,31}?)\\] *)?(?:year *= *\\[ *(\\d+) *\\] *)?(?:number *= *\\[ *(\\d+) *\\] *)?(?>journal *= *\\[(.{1,63}?)\\] *)?(?>article *= *\\[(.{1,127}?)\\] *)?");
+const boost::regex re_remove(" *remove *ID *= *\\[ *(\\d+) *\\] *");
+const boost::regex re_insert(" *insert *(?>author *= *\\[(.{1,31}?)\\] *)(?:year *= *\\[ *(\\d+) *\\] *)(?:number *= *\\[ *(\\d+) *\\] *)(?>journal *= *\\[(.{1,63}?)\\] *)(?>article *= *\\[(.{1,127}?)\\] *)");
   
   //  vadelyaem pamyat i cosdaem object
 
@@ -172,7 +189,7 @@ void dump(void)  //  write v files iz "data"
 	printf("Dump end\n");
 }
 
-void load(void)  //  read v "datA"  IZ filov
+void load(void)  //  read v "data"  IZ filov
 {
 	printf("Load start\n");
 	FILE * fmeta = fopen(fnamemeta, "r");
@@ -214,7 +231,7 @@ void load(void)  //  read v "datA"  IZ filov
 		strmap_vtype elem;
 		fread(&l, 1, 1, fathrmap);
 		fread(buff, l, 1, fathrmap);
-		buff[l] = '\0';
+		buff[(int32_t)l] = '\0';
 		elem.first = std::string(buff);
 		fread(&elem.second, sizeof(elem.second), 1, fathrmap);
 		athrmap.insert(elem);
@@ -369,42 +386,40 @@ keysearch_t searchn(int a, int b)
     return res;
 }
 
-response_t execute_query(char * query)
+response_t execute_select(const boost::cmatch & match_res)
 {
-	
-	boost::regex pattern(select_regex);
-    boost::cmatch mres;
-    if (!boost::regex_match(query, mres, pattern))
-    	return std::make_pair<record_t *, int32_t> (nullptr, -1);
-    std::vector<keysearch_t> sets;
-    if (mres[query_pos_author].matched)
+	rwmutex.lock_shared();
+	std::vector<keysearch_t> sets;
+    if (match_res[select_pos_author].matched)
     {
-    	sets.push_back(searcha((std::string)mres[query_pos_author]));
+    	sets.push_back(searcha((std::string)match_res[select_pos_author]));
     }
-    if (mres[query_pos_year1].matched)
+    if (match_res[select_pos_year1].matched)
     {
     	int left, right;
     	try
     	{
-    		left = std::stoi((std::string)mres[query_pos_year1]);
-    		right = std::stoi((std::string)mres[query_pos_year2]);
+    		left = std::stoi((std::string)match_res[select_pos_year1]);
+    		right = std::stoi((std::string)match_res[select_pos_year2]);
     	}
     	catch (const std::out_of_range & ex)
     	{
+    		rwmutex.unlock_shared();
     		return std::make_pair<record_t *, int32_t> (nullptr, -2);
     	}
     	sets.push_back(searchy(left, right));
     }
-    if (mres[query_pos_number1].matched)
+    if (match_res[select_pos_number1].matched)
     {
     	int left, right;
     	try
     	{
-    		left = std::stoi((std::string)mres[query_pos_number1]);
-    		right = std::stoi((std::string)mres[query_pos_number2]);
+    		left = std::stoi((std::string)match_res[select_pos_number1]);
+    		right = std::stoi((std::string)match_res[select_pos_number2]);
     	}
     	catch (const std::out_of_range & ex)
     	{
+    		rwmutex.unlock_shared();
     		return std::make_pair<record_t *, int32_t> (nullptr, -3);
     	}
     	sets.push_back(searchn(left, right));
@@ -420,7 +435,9 @@ response_t execute_query(char * query)
     		else
     			--j;
     	}
-    	return std::make_pair<record_t *, int32_t> (std::move(result), keysmap.size());
+    	sz = keysmap.size();
+    	rwmutex.unlock_shared();
+    	return std::make_pair<record_t *, int32_t> (std::move(result), std::move(sz));
     }
     if (sets.size() == 1)
     {
@@ -431,12 +448,13 @@ response_t execute_query(char * query)
     	{
     		result[i] = data[keysmap[*it]];
     	}
+    	rwmutex.unlock_shared();
     	return std::make_pair<record_t *, int32_t> (std::move(result), std::move(sz));
     }
     if (sets.size() > 1)
     {
     	keysearch_t tmp;
-    	for (int i = 1; i < sets.size(); ++i)
+    	for (size_t i = 1; i < sets.size(); ++i)
     	{
     		tmp.clear();
     		set_intersection(sets[0].begin(), sets[0].end(),
@@ -451,10 +469,156 @@ response_t execute_query(char * query)
     	{
     		result[i] = data[keysmap[*it]];
     	}
+    	rwmutex.unlock_shared();
     	return std::make_pair<record_t *, int32_t> (std::move(result), std::move(sz));
     }
     // Unreachable 
+    rwmutex.unlock_shared();
     return std::make_pair<record_t *, int32_t> (nullptr, -4);
+}
+
+response_t execute_remove(const boost::cmatch & match_res)
+{
+	rwmutex.lock();
+	int64_t ID;
+   	try
+   	{
+   		ID = std::stoll((std::string)match_res[remove_pos_ID]);
+   	}
+   	catch (const std::out_of_range & ex)
+   	{
+   		rwmutex.unlock();
+   		return std::make_pair<record_t *, int32_t> (nullptr, -2);
+   	}
+   	if (keysmap.find(ID) != keysmap.end())
+   		remove(ID);
+   	rwmutex.unlock();
+   	return std::make_pair<record_t *, int32_t> (nullptr, 0);
+}
+
+response_t execute_update(const boost::cmatch & match_res)
+{
+	rwmutex.lock();
+	int64_t ID;
+   	try
+   	{
+   		ID = std::stoll((std::string)match_res[update_pos_ID]);
+   	}
+   	catch (const std::out_of_range & ex)
+   	{
+   		rwmutex.unlock();
+   		return std::make_pair<record_t *, int32_t> (nullptr, -2);
+   	}
+   	auto it = keysmap.find(ID);
+   	if (it == keysmap.end())
+   	{
+   		rwmutex.unlock();
+   		return std::make_pair<record_t *, int32_t> (nullptr, 0);
+   	}
+
+   	record_t record = data[it->second];
+   	if (match_res[update_pos_author].matched)
+    {
+    	std::copy(match_res[update_pos_author].first,
+    			  match_res[update_pos_author].second,
+    			  record.author);
+    	record.author[match_res[update_pos_author].length()] = '\0';
+    }
+    if (match_res[update_pos_year].matched)
+    {
+    	int year;
+   		try
+   		{
+   			year = std::stoi((std::string)match_res[update_pos_year]);
+   		}
+   		catch (const std::out_of_range & ex)
+   		{
+   			rwmutex.unlock();
+   			return std::make_pair<record_t *, int32_t> (nullptr, -2);
+   		}
+   		record.year = year;
+    }
+    if (match_res[update_pos_number].matched)
+    {
+    	int number;
+   		try
+   		{
+   			number = std::stoi((std::string)match_res[update_pos_number]);
+   		}
+   		catch (const std::out_of_range & ex)
+   		{
+   			rwmutex.unlock();
+   			return std::make_pair<record_t *, int32_t> (nullptr, -2);
+   		}
+   		record.number = number;
+    }
+    if (match_res[update_pos_journal].matched)
+    {
+    	std::copy(match_res[update_pos_journal].first,
+    			  match_res[update_pos_journal].second,
+    			  record.journal);
+    	record.journal[match_res[update_pos_journal].length()] = '\0';
+    }
+    if (match_res[update_pos_article].matched)
+    {
+    	std::copy(match_res[update_pos_article].first,
+    			  match_res[update_pos_article].second,
+    			  record.article);
+    	record.article[match_res[update_pos_article].length()] = '\0';
+    }
+    update(ID, record);
+    rwmutex.unlock();
+    return std::make_pair<record_t *, int32_t> (nullptr, 0);
+}
+
+response_t execute_insert(const boost::cmatch & match_res)
+{
+	record_t record;
+    std::copy(match_res[insert_pos_author].first,
+    		  match_res[insert_pos_author].second,
+    		  record.author);
+    record.author[match_res[insert_pos_author].length()] = '\0';
+    int year, number;
+   	try
+   	{
+   		year = std::stoi((std::string)match_res[insert_pos_year]);
+   		number = std::stoi((std::string)match_res[insert_pos_number]);
+   	}
+   	catch (const std::out_of_range & ex)
+   	{
+   		rwmutex.unlock();
+   		return std::make_pair<record_t *, int32_t> (nullptr, -2);
+   	}
+   	record.year = year;
+   	record.number = number;
+    std::copy(match_res[insert_pos_journal].first,
+    		  match_res[insert_pos_journal].second,
+    		  record.journal);
+    record.journal[match_res[insert_pos_journal].length()] = '\0';
+    std::copy(match_res[insert_pos_article].first,
+    		  match_res[insert_pos_article].second,
+    		  record.article);
+    record.article[match_res[insert_pos_article].length()] = '\0';
+    int64_t ID = ++max_ID;
+    rwmutex.lock();
+    insert(ID, record);
+    rwmutex.unlock();
+    return std::make_pair<record_t *, int32_t> (nullptr, 0);
+}
+
+response_t execute_query(char * query)
+{
+	
+	boost::cmatch mres;
+	if (boost::regex_match(query, mres, re_select))
+		return execute_select(mres);
+	if (boost::regex_match(query, mres, re_remove))
+		return execute_remove(mres);
+	if (boost::regex_match(query, mres, re_update))
+		return execute_update(mres);
+	if (boost::regex_match(query, mres, re_insert))
+		return execute_insert(mres);
+    return std::make_pair<record_t *, int32_t> (nullptr, -1);
     // Do C++ programs dream of template memes?
 }
 
@@ -462,26 +626,77 @@ void thread_fun(int tid, int sockid)   //  poluchaet  ID potoka (1 / 16) i  ID s
 {
 
     int chk = 1;
-    sendall(sockid, (char *)&chk, sizeof(int), MSG_NOSIGNAL);
-    recvall(sockid, (char *)&chk, sizeof(int), MSG_NOSIGNAL);
     char com[max_com_len];
+    if (sendall(sockid, (char *)&chk, sizeof(int), MSG_NOSIGNAL) < 0)
+    {
+    	sprintf(com, "send one tid = %2d", tid);
+    	perror(com);
+    	shutdown(sockid, 2);
+    	close(sockid);
+	
+		thread_edit_mutex.lock();
+		thread_ids.push_back(tid);
+		thread_run_mutex.unlock();
+		thread_edit_mutex.unlock();
+    	return;
+    }
+
+    if (recvall(sockid, (char *)&chk, sizeof(int), MSG_NOSIGNAL) < 0)
+    {
+    	sprintf(com, "recv one tid = %2d", tid);
+    	perror(com);
+    	shutdown(sockid, 2);
+    	close(sockid);
+	
+		thread_edit_mutex.lock();
+		thread_ids.push_back(tid);
+		thread_run_mutex.unlock();
+		thread_edit_mutex.unlock();
+    	return;
+    }
+    
     
     while (true)
     {
     	int32_t len;
-        recvall(sockid, (char *)&len, sizeof(int), MSG_NOSIGNAL); //without null-terminator at the end of command
+        if (recvall(sockid, (char *)&len, sizeof(int), MSG_NOSIGNAL) < 0) //without null-terminator at the end of command
+        {
+        	sprintf(com, "recv len tid = %2d", tid);
+    		perror(com);
+    		break;
+        } 
         if (len == 0)
         	break;
     	if (chk)
     		len = swapbytes(len);
     	if (len > max_com_len)
     		len = max_com_len;
-    	recvall(sockid, com, len, MSG_NOSIGNAL);
+    	if (recvall(sockid, com, len, MSG_NOSIGNAL) < 0)
+    	{
+    		sprintf(com, "recv com tid = %2d", tid);
+    		perror(com);
+    		break;
+    	}
     	com[len] = '\0';
     	response_t response = execute_query(com);
-    	sendall(sockid, (char *)&response.second, sizeof(int), MSG_NOSIGNAL);
+    	if (sendall(sockid, (char *)&response.second, sizeof(int), MSG_NOSIGNAL) < 0)
+    	{
+    		sprintf(com, "send len tid = %2d", tid);
+    		perror(com);
+    		free(response.first);
+    		response.first = nullptr;
+    		break;
+    	}
     	if (response.second >= 0)
-    		sendall(sockid, (char *)response.first, response.second * sizeof(record_t), MSG_NOSIGNAL);
+    		if (sendall(sockid, (char *)response.first, response.second * sizeof(record_t), MSG_NOSIGNAL) < 0)
+    		{
+    			sprintf(com, "send res tid = %2d", tid);
+    			perror(com);
+    			free(response.first);
+    			response.first = nullptr;
+    			break;
+    		}
+
     	free(response.first);
     	response.first = nullptr;
 #ifdef MYAPP_VERSION_1
@@ -528,36 +743,33 @@ std::vector<record_t> generate(int count, int art, int aut, int jou, int yearmin
 	return res;
 }
 
-int main(int argc, char const *argv[])
+void ggen (void)
 {
-	if (!(max_thread = std::thread::hardware_concurrency()))
-		max_thread = 16;
-	tcp_port = 55555;
-	for (int i = 0; i < max_thread; ++i)
-	{
-		thread_ids.push_back(i);
-	}
+	std::vector<record_t> gen = generate(1, 200000, 10000, 1000, 1900, 2000, 1, 1000);
 
-	/*
-	std::vector<record_t> gen = generate(300000, 200000, 10000, 1000, 1900, 2000, 1, 1000);
-
-	for (int i = 0; i < 300000; ++i)
+	for (int i = 0; i < 1; ++i)
 	{
 		insert(gen[i].ID, gen[i]);
 		if (!(i % 1000))
 			std::cout << i << std::endl;
 	}
-	max_ID = 300000;
+	max_ID = 1;
 	dump();
+}
 
-	return 0;
-	*/
-	load();
-
-	for (auto it = athrmap.begin(); it != athrmap.end(); ++it)
+int main(int argc, char const *argv[])
+{
+	if (!(max_thread = std::thread::hardware_concurrency()))
+		max_thread = 16;
+	tcp_port = 55555;
+	for (size_t i = 0; i < max_thread; ++i)
 	{
-		/* code */
+		thread_ids.push_back(i);
 	}
+
+	//ggen(); return 0;
+
+	load();
 
 	int a = 5;
 	printf("%d\n", a);
